d3a2226d90a6c5824bf6f388b571fd795ae93d24
Dylan Langston <dylanlangston@gmail.com>
2023-11-11 01:28:10 -0500

    Updated Svelte Site looks a tiny bit

--- src/scale-website/src/routes/+page.svelte
+++ src/scale-website/src/routes/+page.svelte
@@ -22,6 +22,9 @@ function loadEmscripten(): void {
   (<any>window).Module = new Module();
   (<any>window).Module.setStatus("Downloading...");
   (<any>window).onerror = (e: any) => {
+    const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    canvas.style.display = 'none';
+
     (<any>window).Module.setStatus("Exception thrown, see JavaScript console");
     (<any>window).Module.setStatus = (e: any) => {
       e && console.error("[post-exception status] " + e);
@@ -29,7 +32,7 @@ function loadEmscripten(): void {
   };
 }
 
-const padding_horizontal = 24;
+const padding_horizontal = 0;
 let updateSizeTimeout: number|undefined = undefined;
 function UpdateSize(e: Event): void {
   if ((<any>Browser).isFullscreen)
@@ -52,17 +55,23 @@ onMount(() => {
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.green.500);
+    background-color: theme(colors.gray.300);
+  }
+
+  .btn-fullscreen {
+    -webkit-text-stroke: 1px black;
+    -webkit-text-fill-color: white;
   }
 </style>
 
-<span id="controls">
-  <button type="button" on:click={() => toggleFullScreen()}>Fullscreen</button>
+<span id="controls" class="absolute right-0 pr-3 pt-2" title="Fullscreen">
+  <button type="button" on:click={() => toggleFullScreen()}><a class="rounded-lg bg-slate-400/[.3] font-extrabold text-3xl btn-fullscreen p-2 pt-0">⛶</a></button>
 </span>
 <div class="emscripten">
-  <progress hidden id="progress" max="100" value="0"/>
+  <canvas class="emscripten w-100 bg-gray-300" id="canvas" on:contextmenu={(e) => e.preventDefault()} tabindex=-1></canvas>
 </div>
-<div class="emscripten">
-  <canvas class="emscripten w-100" id="canvas" on:contextmenu={(e) => e.preventDefault()} tabindex=-1></canvas>
+<div class="absolute flex top-0 bottom-0 left-0 right-0 items-center justify-center pointer-events-none -z-50">
+  <div class="rounded-lg bg-slate-50 shadow-xl p-8">
+    <div class="emscripten text-center text-6xl font-bold" id="status">Downloading...</div>
+  </div>
 </div>
-<div class="emscripten" id="status">Downloading...</div>
